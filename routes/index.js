b611e16892b80cfd5feca621fe3bd568889729dc
Gergely Revay <gergely.revay@gmail.com>
2012-06-12 08:11:31 -0700

    Added new routes to upload.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+exports.view = function(req, res){
+  res.render('viewPhotos', {title: 'View'})
 };
+
+exports.upload = function(req, res){
+  res.render('uploadPhotos', {title: 'View'})
+};
+
+/*exports.savePhoto = function(req, res){
+    photoStore.save(req, function(err){
+    res.redirect('/');
+  });
+};
+*/
